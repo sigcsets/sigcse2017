7e2364446a292d7fa59b65d5eb8eb996e05cafbc
Matt Jadud <matt@jadud.com>
2016-06-21 21:06:54 -0400

    Wording change, new upload script.
    Made a wording change (papers -> participation), and added a live upload script.

--- _tools/upload.bash
+++ _tools/upload.bash
@@ -11,24 +11,16 @@ function upload {
     sigcse@sigcse.hosting.acm.org:/home/sigcse/www/$1
 }
 
-function jadudstaging {
-  rsync -vrz \
-    -e "ssh -i /Users/jadudm/.ssh/big-mac-berea" _site/ \
-    jadudm@jadud.com:/home/jadudm/jadud.com/.sigcse2017
-}
-
 # First, remove the old site.
 if [ -d _site ];
 then
   rm -rf _site/
 fi
 
-# If they want to upload the staging site, lets do that.
-if [ "$1" == "jadud" ];
+if [ "$1" == "live" ] || [ "$1" == "all" ];
 then
-  build _config_jadud.yaml
-  ./_tools/minify.bash
-  jadudstaging
+  build _config_live.yaml
+  upload sigcse2017/
 fi
 
 # If they want to upload the staging site, lets do that.
@@ -37,10 +29,3 @@ then
   build _config_staging.yaml
   upload sigcse2017/.staging
 fi
-
-# if [ $1 == "production" ] || [ $1 == "all" ];
-# then
-#   rsync -vrz \
-#     -e "ssh -p 7822 -i /Users/jadudm/.ssh/big-mac-berea" _site/ \
-#     sigcse@sigcse.hosting.acm.org:/home/sigcse/www/sigcse2016
-# fi
